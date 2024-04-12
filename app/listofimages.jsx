2047bae1dee2765dc93fa4ee5119c1906543f5f7
AJ <ajblankenship1@gmail.com>
2024-04-12 11:40:15 +0000

    second commit

---
+++ app/listofimages.jsx
@@ -0,0 +1,22 @@
+export default [
+    "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/9.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/10.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/10.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/9.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+]
--- app/page.js
+++ app/page.js
@@ -2,56 +2,66 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import rickandmortyimage from "/home/ubuntu/Desktop/nextjs-rick-and-morty/public/rick-and-morty-characters.jpeg";
-
+import styles from "@/app/page.module.css";
+import listofimages from "./listofimages";
 
 
 export default function Home() {
     const [loading, setLoading] = useState(true);
     const [character, setCharacter] = useState("");
-    const [characterIndex, setCharacterIndex] = useState(0);
-    const [imageToDisplay, setImageToDisplay] = useState("https://rickandmortyapi.com/api/character/avatar/1.jpeg");
-   
+    const[showCard, setShowCard] = useState (styles.showCard);
+
     useEffect(()=>{
         getCharacter();
         async function getCharacter(){
             const response = await fetch("https://rickandmortyapi.com/api/character",{method: "GET"});
             const data = await response.json();
             
-            setCharacter(data.results[0].name);
+            setCharacter(data.results[1].name);
             setLoading(false);
 
         }   
     }, []);
         
         
-function CharacterName(){
-    return (
-        <div>
-            <p>{loading}</p>
-            <h2>{character}</h2>
-        </div>
-    )
-}
 
-function CharacterImg(){
-    return (
-        <div>
-            <Image
-            src={imageToDisplay}
-            alt="Picture Rick and Morty Characters"
-            height={500}
-            width={500}
-            priority={true}
-            />
+
+
+function Card(props){
+    const[showCard, setShowCard] = useState(false);
+    
+    function handleCardClick(){
+        setShowCard(!showCard);
+    }
+
+    const {imageSrc} = props;
+    return(
+        <div className={styles.showBackOfCard}>
+            <li className={showCard ? styles.showCard : styles.hideCard} onClick={handleCardClick}>  
+                <Image
+                    src={imageSrc}
+                    alt="Picture Rick and Morty Characters"
+                    height={200}
+                    width={170}
+                    priority={true}
+                />
+            </li>
         </div>
-    )
-}
+)}
+
 
     return (
         <main>
-           <h1>Rick and Morty Characters!</h1>
-            <CharacterName/>
-            <CharacterImg/>
+           <h1>Rick and Morty Memory Game!</h1>
+           <section className={styles.gameBoard}>
+                <ul>
+                {listofimages.map((item, index) => {
+                            return (
+                               <Card imageSrc={item} key={index}/>
+                        )})}
+                    </ul>
+           </section>
+            
         </main>
     );
 }

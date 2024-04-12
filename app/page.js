3af5c3189dab1cd87bfe5895fac27d4adaeb1c75
AJ <ajblankenship1@gmail.com>
2024-04-12 04:22:59 +0000

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,57 @@
+'use client'
+import { useState, useEffect } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+import rickandmortyimage from "/home/ubuntu/Desktop/nextjs-rick-and-morty/public/rick-and-morty-characters.jpeg";
+
+
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    const [loading, setLoading] = useState(true);
+    const [character, setCharacter] = useState("");
+    const [characterIndex, setCharacterIndex] = useState(0);
+    const [imageToDisplay, setImageToDisplay] = useState("https://rickandmortyapi.com/api/character/avatar/1.jpeg");
+   
+    useEffect(()=>{
+        getCharacter();
+        async function getCharacter(){
+            const response = await fetch("https://rickandmortyapi.com/api/character",{method: "GET"});
+            const data = await response.json();
+            
+            setCharacter(data.results[0].name);
+            setLoading(false);
+
+        }   
+    }, []);
+        
+        
+function CharacterName(){
+    return (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <p>{loading}</p>
+            <h2>{character}</h2>
         </div>
-      </div>
+    )
+}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function CharacterImg(){
+    return (
+        <div>
+            <Image
+            src={imageToDisplay}
+            alt="Picture Rick and Morty Characters"
+            height={500}
+            width={500}
+            priority={true}
+            />
+        </div>
+    )
+}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+           <h1>Rick and Morty Characters!</h1>
+            <CharacterName/>
+            <CharacterImg/>
+        </main>
+    );
 }
